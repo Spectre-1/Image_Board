a9506a456249763768041f0939590d5c8f219682
Spectre <pgudijan@uci.edu>
2023-06-03 11:54:26 -0700

    Basic GUI

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@ import os
 import requests
 import time
 from dotenv import load_dotenv
+import tkinter as tk
+from tkinter import messagebox
+
 
 load_dotenv("./secret/secret.env")
 
@@ -37,4 +40,33 @@ api_key = os.getenv("API_KEY")
 tag = "cat_ears"
 number_of_images = 10
 
-download_images(api_key, tag, number_of_images)
+# download_images(api_key, tag, number_of_images)
+
+def download_images_gui(api_key):
+    tag = tag_entry.get()
+    number_of_images = int(num_images_entry.get())
+    try:
+        download_images(api_key, tag, number_of_images)
+        messagebox.showinfo("Success", f"Successfully downloaded {number_of_images} images with tag {tag}")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+root = tk.Tk()
+root.geometry('300x200')
+
+tag_label = tk.Label(root, text="Enter tag")
+tag_label.pack()
+
+tag_entry = tk.Entry(root)
+tag_entry.pack()
+
+num_images_label = tk.Label(root, text="Enter number of images")
+num_images_label.pack()
+
+num_images_entry = tk.Entry(root)
+num_images_entry.pack()
+
+download_button = tk.Button(root, text="Download Images", command=lambda: download_images_gui(api_key))
+download_button.pack()
+
+root.mainloop()
